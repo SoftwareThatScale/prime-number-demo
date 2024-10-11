6ddc43734356f560413f2eb721727dd8ad0bef4d
Benoit Tremblay <trembl.ben@gmail.com>
2024-10-11 10:16:15 -0400

    Add custom metrics

--- app/src/prime.ts
+++ app/src/prime.ts
@@ -1,4 +1,17 @@
+import { metrics } from "@opentelemetry/api";
+
+const meter = metrics.getMeter('prime');
+const checkCounter = meter.createCounter('prime.check.counter', {
+  description: 'Number of check if a number is prime'
+});
+
+const primeCounter = meter.createCounter('prime.counter', {
+  description: 'Number of prime numbers found'
+})
+
 function isPrimeNumber(number: number): boolean {
+  checkCounter.add(1);
+
   if (number <= 1) {
     return false;
   }
@@ -13,6 +26,8 @@ function isPrimeNumber(number: number): boolean {
 }
 
 export function findPrimeNumbers(amount: number): Array<number> {
+  primeCounter.add(amount);
+
   const values: Array<number> = [];
 
   let currentValue = 2;
